741046b81e6891efcb2268e26800c15f95446f8e
gabrielnakhata <gabrielnakata@gmail.com>
2021-09-04 00:27:09 -0300

    Implementando cadastro de categoria...

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sistema_loja_venda.Controllers.DAL;
+using sistema_loja_venda.Entidades;
+using sistema_loja_venda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_loja_venda.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        protected ApplicationDbContext mContext;
+
+        public CategoriaController(ApplicationDbContext context)
+        {
+            mContext = context; 
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> lista = mContext.Categoria.ToList();
+            mContext.Dispose();
+            return View(lista);
+        }
+
+        [HttpGet]
+        public IActionResult Cadastro(int? id)
+        {
+            CategoriaViewModel viewModel = new CategoriaViewModel();
+
+            return View(viewModel);
+        }
+    }   
+}
---
+++ Models/CategoriaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_loja_venda.Models
+{
+    public class CategoriaViewModel
+    {
+        public int? Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}

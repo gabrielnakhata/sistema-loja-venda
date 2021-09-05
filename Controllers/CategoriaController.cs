8d6dc050b1ccedde2f5159228677cd6a0c6b1337
gabrielnakhata <gabrielnakata@gmail.com>
2021-09-05 01:05:21 -0300

    CRUD completo, cadastro de categoria...

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using sistema_loja_venda.Controllers.DAL;
 using sistema_loja_venda.Entidades;
 using sistema_loja_venda.Models;
@@ -29,7 +30,55 @@ namespace sistema_loja_venda.Controllers
         {
             CategoriaViewModel viewModel = new CategoriaViewModel();
 
+            if (id != null)
+            {
+                var entidade = mContext.Categoria.Where(x => x.Codigo == id).FirstOrDefault();
+                viewModel.Codigo = entidade.Codigo;
+                viewModel.Descricao = entidade.Descricao;
+            }
+
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(CategoriaViewModel entidade)
+        {
+            if (ModelState.IsValid)
+            {
+                Categoria objCategoria = new Categoria()
+                {
+                    Codigo = entidade.Codigo,
+                    Descricao = entidade.Descricao
+                };
+
+                if (entidade.Codigo == null)
+                {
+                    mContext.Categoria.Add(objCategoria);
+                }
+                else
+                {
+                    mContext.Entry(objCategoria).State = EntityState.Modified;
+                }
+
+                mContext.SaveChanges();
+            }
+            else
+            {
+                return View(entidade);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+
+        public IActionResult Excluir(int id)
+        {
+            var ent = new Categoria() { Codigo = id };
+            mContext.Attach(ent);
+            mContext.Remove(ent);
+            mContext.SaveChanges();
+            return RedirectToAction("Index");
         }
     }   
 }
--- Models/CategoriaViewModel.cs
+++ Models/CategoriaViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace sistema_loja_venda.Models
     public class CategoriaViewModel
     {
         public int? Codigo { get; set; }
+
+        [Required(ErrorMessage="Informe a descrição da categoria!")]
         public string Descricao { get; set; }
     }
 }

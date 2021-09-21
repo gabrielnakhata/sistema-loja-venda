aad5e41069e226c07595a170e043508c12e04aa6
Gabriel Nakhata <gabrielnakata@gmail.com>
2021-09-20 23:52:53 -0300

    Enviando código para repositório...

---
+++ Controllers/RelatorioController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sistema_loja_venda.Controllers.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_loja_venda.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        protected ApplicationDbContext mContext;
+        public RelatorioController(ApplicationDbContext context)
+        {
+            mContext = context;
+        }
+
+        public IActionResult Grafico()
+        {
+            return View();
+        }
+    }
+}

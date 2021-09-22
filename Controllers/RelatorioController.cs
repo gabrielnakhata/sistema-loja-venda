b5bd1c8a5142ad7a9b230cd0025ad5aa3359b9ea
Gabriel Nakhata <gabrielnakata@gmail.com>
2021-09-22 00:56:33 -0300

    Enviando código para repositório...

--- Controllers/RelatorioController.cs
+++ Controllers/RelatorioController.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using sistema_loja_venda.Controllers.DAL;
+using sistema_loja_venda.Entidades;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace sistema_loja_venda.Controllers
 {
     public class RelatorioController : Controller
-    {
+    {   
         protected ApplicationDbContext mContext;
         public RelatorioController(ApplicationDbContext context)
         {
@@ -17,6 +16,31 @@ namespace sistema_loja_venda.Controllers
 
         public IActionResult Grafico()
         {
+            var lista = mContext.Venda_Produtos
+                .GroupBy(x => x.Codigo_produto)
+                .Select(y => new GraficoViewModel
+                {
+                    Codigoproduto = y.First().Codigo_produto,
+                    Descricao = y.First().Produto.Descricao,
+                    Totalvendido = y.Sum(z => z.Quantidade)
+                }).ToList();
+
+            string valores = string.Empty;
+            string labels = string.Empty;
+            string cores = string.Empty;
+
+            var random = new Random();
+            for (int i = 0; i < lista.Count; i++)
+            {
+                valores += lista[i].Totalvendido.ToString() + ",";
+                labels += "'" + lista[i].Descricao.ToString() + ",";
+                cores += "' " + String.Format("#{0:X6}", random.Next(i, i * 2)) + "',";
+            }
+
+            ViewBag["valores"] = valores;
+            ViewBag["labels"] = labels;
+            ViewBag["cores"] = cores; 
+            
             return View();
         }
     }
---
+++ Models/GraficoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_loja_venda.Entidades
+{
+    public class GraficoViewModel
+    {
+        public int Codigoproduto { get; set; }
+        public string Descricao { get; set; }
+        public double Totalvendido { get; set; }
+
+    }
+}
